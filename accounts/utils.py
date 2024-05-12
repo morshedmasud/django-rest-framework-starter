04019d239b61a436c0146b377e3754d07f7e3991
Md Morshed Alam <morshed.dev@gmail.com>
2024-05-12 23:56:56 +0600

    fix: dependabot alerts addressed

--- accounts/utils.py
+++ accounts/utils.py
@@ -4,6 +4,11 @@ from django.core.mail import EmailMessage
 class Util:
     @staticmethod
     def send_email(data):
-        email = EmailMessage(to=[data['to_email']], subject=data['email_subject'], body=data['email_body'])
+        try:
+            email = EmailMessage(to=[data['to_email']], subject=data['email_subject'], body=data['email_body'])
 
-        email.send()
+            email.send()
+        except Exception as err:
+            print(f"raised error while sending email: {err}")
+
+
